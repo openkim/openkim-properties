b365d19f3c23479ab36782f78be58913115cae3c
openkim-jenkins <openkim-jenkins>
2019-01-15 12:36:13 -0600

    update dependencies

--- definition-validator/project.clj
+++ definition-validator/project.clj
@@ -1,10 +1,10 @@
 (defproject definition-validator "1.0.0"
   :description "KIM Property Definition Validator"
   :url "https://github.com/openkim/openkim-properties"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [prismatic/schema "1.1.0"]
-                 [org.clojure/tools.cli "0.3.3"]
-                 [fipp "0.6.4"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [prismatic/schema "1.1.9"]
+                 [org.clojure/tools.cli "0.4.1"]
+                 [fipp "0.6.14"]
                  [me.raynes/fs "1.4.6"]]
   :target-path ".target"
   :compile-path ".target/classes"
--- instance-validator/project.clj
+++ instance-validator/project.clj
@@ -1,16 +1,16 @@
 (defproject instance-validator "1.0.0"
   :description "KIM Property Instance Validator"
   :url "https://github.com/openkim/openkim-properties"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [prismatic/schema "1.1.0"]
-                 [org.clojure/tools.cli "0.3.3"]
-                 [fipp "0.6.4"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [prismatic/schema "1.1.9"]
+                 [org.clojure/tools.cli "0.4.1"]
+                 [fipp "0.6.14"]
                  [me.raynes/fs "1.4.6"]
                  ;;[net.mikera/core.matrix "0.24.0"]
-                 [compojure "1.5.0"]
-                 [ring "1.4.0"]
-                 [cheshire "5.5.0"]
-                 [ns-tracker "0.3.0"]]
+                 [compojure "1.6.1"]
+                 [ring "1.7.1"]
+                 [cheshire "5.8.1"]
+                 [ns-tracker "0.3.1"]]
   :target-path ".target"
   :compile-path ".target/classes"
   :resource-paths ["test/resources"]
